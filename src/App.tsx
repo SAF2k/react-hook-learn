73e8cd4e34533f39363f0809eafb96070de2938d
SAFWAT <safwathkhan2@gmail.com>
2023-08-29 20:46:25 +0530

    added welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Counter from "./components/Counter";
+import Home from "./components/Home";
 import NotCounter from "./components/NotContore";
 
 function App() {
   return (
     <>
       <div className="">
+        <Home />
         <Counter />
         <NotCounter />
       </div>
